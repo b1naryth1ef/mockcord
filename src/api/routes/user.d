8c39facedb9bb01a14a80a11c0ec34c96b88cc81
Andrei <b1naryth1ef@gmail.com>
2017-02-13 01:04:31 -0800

    more shtuff

--- src/api/routes/user.d
+++ src/api/routes/user.d
@@ -1,13 +1,14 @@
 module mockcord.api.routes.user;
 
+import std.conv : to;
+import std.format : format;
+
 import mockcord.api.routes;
 
-
-import dscord.util.json : serializeToJSON;
-import dscord.types.user : User;
+import dscord.util.json : VibeJSON, serializeToJSON;
+import dscord.types : Snowflake, User;
 
 import std.stdio;
-
 
 class UserRoutes {
   mixin Routes;
@@ -22,10 +23,33 @@ class UserRoutes {
 
     if (userID == "@me") {
       res.writeJsonBody(state.me.serializeToJSON);
-    } else {
-      // TODO
+      res.statusCode = 200;
+      return;
     }
 
-    res.statusCode = 200;
+    Snowflake realUserID;
+
+    try {
+      realUserID = userID.to!Snowflake;
+    } catch (Throwable) {
+      VibeJSON obj = VibeJSON.emptyObject;
+      obj["user_id"] = VibeJSON([VibeJSON(format("Value \"%s\" is not snowflake.", userID))]);
+      res.writeJsonBody(obj);
+      res.statusCode = 400;
+      return;
+    }
+
+    if (state.users.has(realUserID)) {
+      res.writeJsonBody(state.users[realUserID].serializeToJSON);
+      res.statusCode = 200;
+      return;
+    }
+
+    res.writeJsonBody(VibeJSON([
+      "code": VibeJSON(10013),
+      "message": VibeJSON("Unknown User"),
+    ]));
+
+    res.statusCode = 400;
   }
 }
--- src/state.d
+++ src/state.d
@@ -1,16 +1,45 @@
 module mockcord.state;
 
-import dscord.types.user : User;
+import std.format : format;
+
+import dscord.types.user : User, UserMap;
+
+enum RANDOM_USER_IDS = [
+  280621246411046922,
+  280621274365952001,
+  280621289658515457,
+  280621304561008640,
+  280621319933132800
+];
 
 class State {
+  /// Current user
   User me;
 
+  /// Random Users
+  UserMap users;
+
   this() {
+    this.users = new UserMap;
+
+    this.registerUsers();
+  }
+
+  private void registerUsers() {
+    // Create this user
     this.me = new User;
     this.me.id = 280519437256228864;
     this.me.username = "Mocky";
     this.me.discriminator = "1234";
     this.me.verified = true;
     this.me.email = "mocky@mockcord.xd";
+    this.users[this.me.id] = this.me;
+
+    foreach (idx, uid; RANDOM_USER_IDS) {
+      this.users[uid] = new User;
+      this.users[uid].id = uid;
+      this.users[uid].username = format("Joe %s", uid);
+      this.users[uid].discriminator = format("%04s", idx + 1);
+    }
   }
 }
